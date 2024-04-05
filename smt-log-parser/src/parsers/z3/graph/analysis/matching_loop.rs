a0b4e1eaa81b39aa54c82d0f3c304d2a6629fe64
ojyrkinen <oskari.jyrkinen@gmail.com>
2024-04-05 09:39:58 +0200

    Implemented GUI for showing nth ML

--- smt-log-parser/src/parsers/z3/graph/analysis/matching_loop.rs
+++ smt-log-parser/src/parsers/z3/graph/analysis/matching_loop.rs
@@ -1,8 +1,24 @@
 use crate::parsers::z3::graph::InstGraph;
+use super::NodeIndex;
 
 
 impl InstGraph {
     pub fn search_matching_loops(&mut self) -> usize {
-        0
+        let n = 6.min(self.raw.graph.node_count());
+        let end_nodes = self.raw.graph.node_indices().take(n).collect::<Vec<NodeIndex>>();
+        self.analysis.matching_loop_end_nodes = Some(end_nodes); 
+        n
+    }
+
+    pub fn found_matching_loops(&self) -> Option<usize> {
+        self.analysis.matching_loop_end_nodes.as_ref().map(|mlen| mlen.len())
+    }
+
+    pub fn get_nth_matching_loop_end_node(&self, n: usize) -> Option<&NodeIndex> {
+        if let Some(nodes) = &self.analysis.matching_loop_end_nodes {
+            nodes.get(n)
+        } else {
+            None
+        }
     }
 } 

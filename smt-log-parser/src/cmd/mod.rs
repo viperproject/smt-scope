f4c0cc0e8dff6e161ce9500ed05fa593ca6f4006
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2024-12-22 21:06:06 +0100

    fmt

--- smt-log-parser/src/cmd/mod.rs
+++ smt-log-parser/src/cmd/mod.rs
@@ -17,7 +17,11 @@ pub fn run() -> Result<(), String> {
             pretty_print,
         } => dependencies::run(logfile, depth, pretty_print)?,
         args::Commands::Stats { logfile, k } => stats::run(logfile, k)?,
-        args::Commands::Test { logfiles, timeout, memory } => test::run(logfiles, timeout, memory)?,
+        args::Commands::Test {
+            logfiles,
+            timeout,
+            memory,
+        } => test::run(logfiles, timeout, memory)?,
         args::Commands::Reconstruct { logfile, clean } => reconstruct::run(logfile, clean)?,
     }
 

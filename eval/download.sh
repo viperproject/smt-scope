46f80d3ccb3524fb0bfced1f94da71a6027f8be5
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2025-01-27 20:26:38 +0100

    Fix bug in eval

---
+++ eval/download.sh
@@ -0,0 +1,15 @@
+DIRNAME="$(realpath "$(dirname "$0")")"
+
+VERSION=${2:-"unknown"}
+mkdir -p "$DIRNAME/data_other/github/$VERSION"
+cd "$DIRNAME/data_other/github/$VERSION"
+gh run download $1
+
+# Extract all downloaded artifacts (e.g. at `carbon-1/carbon-1.tar.bz2`)
+ALL=$(find . -name '*.tar.bz2')
+for file in $ALL; do
+    tar -xjf $file
+    rm -rf $(dirname $file)
+done
+
+mv eval/data/* .

18a7b5f5d753aead2e90cff296538f34a5f14058
JonasAlaif <JonasAlaif@users.noreply.github.com>
2025-03-19 17:14:37 +0000

    deploy: 350af707bbe70cdb146840712422b03169ba284f

--- canary/worker.js
+++ canary/worker.js
@@ -249,7 +249,7 @@ let wasm_bindgen;
         return cachedDataViewMemory0;
     }
     function __wbg_adapter_18(arg0, arg1, arg2) {
-        wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc74157ab17d0f22e(arg0, arg1, addHeapObject(arg2));
+        wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h94130b445ca5a8ec(arg0, arg1, addHeapObject(arg2));
     }
 
     function __wbg_adapter_21(arg0, arg1, arg2) {
